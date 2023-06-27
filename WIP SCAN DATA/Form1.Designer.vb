4e3cf447944f68a1c5bdd19c3aed467597df3df0
teerapongchaemchamrat <juejuechaemchamrat@gmail.com>
2023-06-27 15:35:33 +0700

    Add project files.

---
+++ WIP SCAN DATA/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ScanWIPBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.App_PURDataSet = New WIP_SCAN_DATA.App_PURDataSet()
+        Me.Scan_WIPTableAdapter = New WIP_SCAN_DATA.App_PURDataSetTableAdapters.Scan_WIPTableAdapter()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.JobDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ItemDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.QuantityDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.RecipientDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DateDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PictureDataGridViewImageColumn = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ScanWIPBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.App_PURDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.JobDataGridViewTextBoxColumn, Me.ItemDataGridViewTextBoxColumn, Me.QuantityDataGridViewTextBoxColumn, Me.RecipientDataGridViewTextBoxColumn, Me.DateDataGridViewTextBoxColumn, Me.PictureDataGridViewImageColumn})
+        Me.DataGridView1.DataSource = Me.ScanWIPBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(27, 49)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(1137, 556)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'ScanWIPBindingSource
+        '
+        Me.ScanWIPBindingSource.DataMember = "Scan_WIP"
+        Me.ScanWIPBindingSource.DataSource = Me.App_PURDataSet
+        '
+        'App_PURDataSet
+        '
+        Me.App_PURDataSet.DataSetName = "App_PURDataSet"
+        Me.App_PURDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'Scan_WIPTableAdapter
+        '
+        Me.Scan_WIPTableAdapter.ClearBeforeFill = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(246, 20)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Search"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(1089, 611)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 31)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Export Excel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(27, 21)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 4
+        '
+        'JobDataGridViewTextBoxColumn
+        '
+        Me.JobDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.JobDataGridViewTextBoxColumn.DataPropertyName = "Job"
+        Me.JobDataGridViewTextBoxColumn.HeaderText = "Job"
+        Me.JobDataGridViewTextBoxColumn.Name = "JobDataGridViewTextBoxColumn"
+        Me.JobDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'ItemDataGridViewTextBoxColumn
+        '
+        Me.ItemDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.ItemDataGridViewTextBoxColumn.DataPropertyName = "Item"
+        Me.ItemDataGridViewTextBoxColumn.HeaderText = "Item"
+        Me.ItemDataGridViewTextBoxColumn.Name = "ItemDataGridViewTextBoxColumn"
+        Me.ItemDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'QuantityDataGridViewTextBoxColumn
+        '
+        Me.QuantityDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.QuantityDataGridViewTextBoxColumn.DataPropertyName = "Quantity"
+        Me.QuantityDataGridViewTextBoxColumn.HeaderText = "Quantity"
+        Me.QuantityDataGridViewTextBoxColumn.Name = "QuantityDataGridViewTextBoxColumn"
+        Me.QuantityDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'RecipientDataGridViewTextBoxColumn
+        '
+        Me.RecipientDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.RecipientDataGridViewTextBoxColumn.DataPropertyName = "Recipient"
+        Me.RecipientDataGridViewTextBoxColumn.HeaderText = "Recipient"
+        Me.RecipientDataGridViewTextBoxColumn.Name = "RecipientDataGridViewTextBoxColumn"
+        Me.RecipientDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'DateDataGridViewTextBoxColumn
+        '
+        Me.DateDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.DateDataGridViewTextBoxColumn.DataPropertyName = "Date"
+        Me.DateDataGridViewTextBoxColumn.HeaderText = "Date"
+        Me.DateDataGridViewTextBoxColumn.Name = "DateDataGridViewTextBoxColumn"
+        Me.DateDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'PictureDataGridViewImageColumn
+        '
+        Me.PictureDataGridViewImageColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.PictureDataGridViewImageColumn.DataPropertyName = "Picture"
+        Me.PictureDataGridViewImageColumn.HeaderText = "Picture"
+        Me.PictureDataGridViewImageColumn.Name = "PictureDataGridViewImageColumn"
+        Me.PictureDataGridViewImageColumn.ReadOnly = True
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.Location = New System.Drawing.Point(1066, 20)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(98, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "LOAD ALL DATA"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1203, 646)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "WIP SCAN DATA"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ScanWIPBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.App_PURDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents App_PURDataSet As App_PURDataSet
+    Friend WithEvents ScanWIPBindingSource As BindingSource
+    Friend WithEvents Scan_WIPTableAdapter As App_PURDataSetTableAdapters.Scan_WIPTableAdapter
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents JobDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents ItemDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents QuantityDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents RecipientDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents DateDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents PictureDataGridViewImageColumn As DataGridViewImageColumn
+    Friend WithEvents Button3 As Button
+End Class
---
+++ WIP SCAN DATA/Form1.vb
@@ -0,0 +1,153 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.Data.SqlClient
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.IO
+Imports OfficeOpenXml
+
+Public Class Form1
+
+    Dim connection As New SqlConnection("Data Source=192.168.10.114\APPSERVER;Initial Catalog=App_PUR;User ID=sa;Password=Cyf027065055")
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to exit?", "Confirm Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If result = DialogResult.No Then
+            e.Cancel = True ' Cancel the closing event
+        End If
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ''TODO: This line of code loads data into the 'App_PURDataSet.Scan_WIP' table. You can move, or remove it, as needed.
+        ''Me.Scan_WIPTableAdapter.Fill(Me.App_PURDataSet.Scan_WIP)
+        'Dim command As New SqlCommand("SELECT DISTINCT Item FROM Scan_WIP", connection)
+        'Dim adapter As New SqlDataAdapter(command)
+        'Dim table As New DataTable()
+        'adapter.Fill(table)
+
+        ''Dim emptyRow As DataRow = table.NewRow()
+        ''emptyRow("Item") = ""
+        ''table.Rows.InsertAt(emptyRow, 0)
+        'ComboBox1.Items.Clear()
+
+        'ComboBox1.DataSource = table
+        'ComboBox1.DisplayMember = "Item"
+        'ComboBox1.ValueMember = "Item"
+
+    End Sub
+
+    Public Sub FilterData(valueToSearch As String)
+        Dim searchQuery As String = "exec WIP_searchDate '" & valueToSearch & "'"
+
+        Dim command As New SqlCommand(searchQuery, connection)
+        Dim adapter As New SqlDataAdapter(command)
+        Dim table As New DataTable()
+
+        adapter.Fill(table)
+        DataGridView1.DataSource = table
+
+        'DataGridView1.RowTemplate.Height = 50
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim selectedDate As DateTime = DateTimePicker1.Value
+        Dim dateString As String = selectedDate.ToString("yyyy-MM-dd")
+
+        FilterData(dateString)
+        'DataGridView1.DefaultCellStyle.WrapMode = DataGridViewTriState.True
+        'DataGridView1.RowTemplate.Height = 150
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ExportToExcel()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Query As String = "SELECT * FROM Scan_WIP"
+        Dim command As New SqlCommand(Query, connection)
+        Dim adapter As New SqlDataAdapter(command)
+        Dim table As New DataTable()
+
+        adapter.Fill(table)
+        DataGridView1.DataSource = table
+
+        'DataGridView1.DefaultCellStyle.WrapMode = DataGridViewTriState.True
+        'DataGridView1.RowTemplate.Height = 150
+    End Sub
+
+    Private Sub ExportToExcel()
+        Dim pictureColumnIndex As Integer = -1
+
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial
+
+        ' Find the index of the "Picture" column
+        For columnIndex As Integer = 0 To DataGridView1.Columns.Count - 1
+            If DataGridView1.Columns(columnIndex).HeaderText = "Picture" Then
+                pictureColumnIndex = columnIndex
+                Exit For
+            End If
+        Next
+
+        If pictureColumnIndex >= 0 Then
+            ' Create a new Excel package
+            Using package As New ExcelPackage()
+                ' Add a new worksheet to the Excel package
+                Dim worksheet As ExcelWorksheet = package.Workbook.Worksheets.Add("Data")
+
+                ' Set the column headers in Excel
+                For columnIndex As Integer = 0 To DataGridView1.Columns.Count - 1
+                    worksheet.Cells(1, columnIndex + 1).Value = DataGridView1.Columns(columnIndex).HeaderText
+                Next
+
+                ' Fix row 1 height
+                worksheet.Row(1).Height = 20
+                worksheet.Column(6).Width = 50
+                ' Define image width and height
+                Dim imageWidth As Integer = 80
+                Dim imageHeight As Integer = 80
+
+                ' Export data from DataGridView to Excel
+                For rowIndex As Integer = 0 To DataGridView1.Rows.Count - 1
+                    Dim row As DataGridViewRow = DataGridView1.Rows(rowIndex)
+                    For columnIndex As Integer = 0 To DataGridView1.Columns.Count - 1
+                        If columnIndex = pictureColumnIndex Then ' Handle the picture column
+                            Dim cellValue As Object = row.Cells(columnIndex).Value
+                            If cellValue IsNot Nothing AndAlso TypeOf cellValue Is Byte() Then
+                                Dim imageBytes As Byte() = DirectCast(cellValue, Byte())
+                                Using imageStream As New MemoryStream(imageBytes)
+                                    ' Add the image to the Excel worksheet
+                                    Dim picture = worksheet.Drawings.AddPicture("picture" & rowIndex, imageStream)
+                                    ' Set the position of the image to the Picture column
+                                    picture.SetPosition(rowIndex + 1, 0, pictureColumnIndex, 0)
+                                    picture.SetSize(imageWidth, imageHeight)
+                                End Using
+                            End If
+                        Else ' Handle other columns
+                            Dim cellValue As Object = row.Cells(columnIndex).Value
+                            worksheet.Cells(rowIndex + 2, columnIndex + 1).Value = If(cellValue IsNot Nothing, cellValue.ToString(), String.Empty)
+                        End If
+                    Next
+
+                    ' Fix row height
+                    worksheet.Row(rowIndex + 2).Height = 80
+                Next
+
+                ' Save the Excel package to a file
+                Dim saveFileDialog As New SaveFileDialog()
+                saveFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx"
+                saveFileDialog.FileName = "ExportedData.xlsx"
+
+                If saveFileDialog.ShowDialog() = DialogResult.OK Then
+                    Dim filePath As String = saveFileDialog.FileName
+                    Dim file As New FileInfo(filePath)
+                    ' Auto-fit columns before saving the Excel package
+                    worksheet.Cells.AutoFitColumns()
+                    package.SaveAs(file)
+                    MessageBox.Show("Data exported to Excel successfully.")
+                End If
+            End Using
+        Else
+            MessageBox.Show("Picture column not found.")
+        End If
+    End Sub
+
+End Class
